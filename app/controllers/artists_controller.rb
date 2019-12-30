d9b643c6e54b2e632fcfe36040cbb15acda3b5b2
RobertTTaylor9517 <robt15@hotmail.com>
2019-12-30 16:36:15 -0500

    artist new page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,35 @@
 class ArtistsController < ApplicationController
+    def new
+        
+    end
+
+    def create
+        @artist = Artist.new(artist_params)
+        if @artist.valid?
+            @artist.save
+            session[:artist_id] = @artist.id
+            redirect_to (@artist)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def select_artist
+        @artist = Artist.find(session[:artist_id])
+    end
+
+    def artist_params
+        params.require(:artist).permit(:name, :user_name, :password, :password_confirmation, :hometown)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :artists, except: [:index, :delete] do
+  resources :artists, except: [:index, :destroy] do
     resources :sessions, except: [:index]
   end
 
