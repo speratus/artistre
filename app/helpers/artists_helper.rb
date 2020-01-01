2c0a1f100bb9ed62a33a289f606cfc39e826691b
RobertTTaylor9517 <robt15@hotmail.com>
2019-12-31 19:13:05 -0500

    fixed errors

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,14 +1,14 @@
 module ArtistsHelper
     def next_session(artist)
        val = get_future_sessions(artist.sessions)
-       val.order('appointment desc').first
+       val.order('appointment asc').first
 
     end
 
     private
 
     def get_future_sessions(sessions)
-        sessions.where("appointment < ?", DateTime.now )
+        sessions.where("appointment > ?", DateTime.now )
     end
 
 end
