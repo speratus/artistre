f8cced047725607c4116da87c31ae0ce1ef05672
RobertTTaylor9517 <robt15@hotmail.com>
2020-01-02 15:45:54 -0500

    pre-final

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt'
 gem 'faker'
 gem 'bulma-rails'
+gem 'material_design_icons'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    # before_action :last_visited
+    after_action :previous_page
+
+    def last_visited
+        @last_page = 
+        byebug
+        session[:current_page] = request.original_fullpath
+        byebug
+    end
+
+    def previous_page
+        unless @performed_redirect
+            session[:previous_page] = request.original_fullpath
+        end
+    end
 end
